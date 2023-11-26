10770509e3f31a10d80c3b2d7d9ff68ceca8ca10
Enzo Quatrochi <enzoquatrochi@outlook.com>
2023-11-26 17:24:25 -0300

    painel administrador concluido

---
+++ Painel administrador/home/home.js
@@ -0,0 +1,42 @@
+var menuItems = document.querySelectorAll('.menu .list-item.parent');
+
+menuItems.forEach(function(menuItem) {
+    menuItem.addEventListener('click', function() {
+        
+        var allSubmenus = document.querySelectorAll('.menu .list-item .submenu');
+        allSubmenus.forEach(function(submenu) {
+            if (submenu !== this.querySelector('.submenu')) {
+                submenu.style.display = 'none';
+            }
+        }, this);
+
+        var submenu = this.querySelector('.submenu');
+        if (submenu) {
+            submenu.style.display = (submenu.style.display === 'block') ? 'none' : 'block';
+        }
+    });
+});
+
+var menu = document.querySelector('.menu');
+
+menu.addEventListener('mouseout', function(e) {
+    
+    var isOverSubmenu = e.relatedTarget && (e.relatedTarget.className === 'submenu' || e.relatedTarget.className === 'sub-item');
+
+    if (!isOverSubmenu) {
+        var allSubmenus = document.querySelectorAll('.menu .list-item .submenu');
+        allSubmenus.forEach(function(submenu) {
+            submenu.style.display = 'none';
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('.sub-item, .parent').forEach(item => {
+        item.addEventListener('click', event => {
+            if (!event.target.querySelector('.submenu')) {
+                window.location.href = event.target.getAttribute('data-href');
+            }
+        });
+    });
+});
